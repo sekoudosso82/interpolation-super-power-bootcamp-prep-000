a337c94250e914b29cf33f0bd126c86f1e6d3568
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-25 05:07:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 # Write your #display_rainbow method here
 ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 def display_rainbow(arg)
-  ""
+  "R: #{arg[]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end 
